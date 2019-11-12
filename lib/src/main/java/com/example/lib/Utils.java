532473421cba17107cf3db71313e748d3b573cf1
Sebastian Glonner <glonner@apt-solutions.de>
2019-11-12 11:58:07 +0100

    add second maven module library

---
+++ lib/src/main/java/com/example/lib/Utils.java
@@ -0,0 +1,7 @@
+package com.example.lib;
+
+public class Utils {
+    static public String version() {
+        return "1.1.0";
+    }
+}
--- webapp/src/main/java/com/example/demo/HttpController.java
+++ webapp/src/main/java/com/example/demo/HttpController.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.lib.Utils;
 
 @RestController
 public class HttpController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "works 133";
+        return "works 133 - " + Utils.version();
     }
 }
